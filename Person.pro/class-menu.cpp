52c8b3b7453605a18d608cbd22ac716d99e746c7
MikolajSiuda <90157619+MikolajSiuda@users.noreply.github.com>
2023-04-13 10:20:14 +0200

    Update class-menu.cpp

--- Person.pro/class-menu.cpp
+++ Person.pro/class-menu.cpp
@@ -2,16 +2,15 @@
 void Menu::menu() {
 	Plik plik;
 	int x = 0;
-	while (x < 7)
+	while (x < 6)
 	{
 		cout << "\n*******************MENU********************" << endl;
 		cout << "1.Dodawanie person" << endl;
 		cout << "2.Wyswietlanie person" << endl;
 		cout << "3.Zmiana person" << endl;
 		cout << "4.Usuwanie person" << endl;
-		cout << "5.Przeladuj pliki" << endl;
-		cout << "6.Szukaj person" << endl;
-		cout << "7.Wyjscie" << endl;
+		cout << "5.Szukaj person" << endl;
+		cout << "6.Wyjscie" << endl;
 		cout << "Wybieram : ";
 		cin >> x;
 		system("cls");
@@ -28,12 +27,9 @@ void Menu::menu() {
 			plik.plik_usun();
 		}
 		else if (x == 5) {
-			plik.plik_przeladuj();
-		}
-		else if (x == 6) {
 			plik.plik_szukaj();
 		}
-		else if (x == 7) {
+		else if (x == 6) {
 			exit(0);
 		}
 	}
